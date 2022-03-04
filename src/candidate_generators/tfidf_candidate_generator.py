853c6f90d78db3eb2b80239fcc362269573ce058
talgatomarov <talgat.omarov.1999@gmail.com>
2022-03-03 21:46:31 -0700

    Add download_artifacts method

---
+++ src/candidate_generators/tfidf_candidate_generator.py
@@ -0,0 +1,67 @@
+import json
+import os
+import tarfile
+
+import gdown
+from src.candidate_generators.base import BaseCandidateEntityGenerator
+from src.data.entity import Entity
+from src.data.mention import Mention
+
+cd = os.path.dirname(os.path.abspath(__file__))
+
+
+class TfidfCandidateGenerator(BaseCandidateEntityGenerator):
+    def __init__(
+        self,
+        entity_dict: dict[str, Entity],
+        filename: str = "train.json",
+        path: str = os.path.join(cd, "artifacts", "tfidf_candidates"),
+    ):
+        BaseCandidateEntityGenerator.__init__(self, entity_dict)
+        self.path = path
+        self.filename = filename
+        self.download_artifacts()
+        self.candidate_dict = self.get_candidate_dict()
+
+    def generate(self, mention: Mention) -> list[Entity]:
+        mention_id = mention.mention_id
+
+        if mention_id not in self.candidate_dict:
+            return []
+
+        candidate_ids = self.candidate_dict[mention_id]
+        candidate_entities = [
+            self.entity_dict[candidate_id] for candidate_id in candidate_ids
+        ]
+
+        return candidate_entities
+
+    def get_candidate_dict(self) -> dict[str, list[str]]:
+        candidate_dict = {}
+        with open(os.path.join(self.path, self.filename), "r") as f:
+            for line in f:
+                line_dict = json.loads(line)
+                candidate_dict[line_dict["mention_id"]] = line_dict[
+                    "tfidf_candidates"
+                ]
+
+        return candidate_dict
+
+    def download_artifacts(self):
+        if os.path.isfile(os.path.join(self.path, self.filename)):
+            return
+
+        url = (
+            "https://drive.google.com/u/0/uc?"
+            "id=1wGppj3ivE7jBaDzDlovWAvaBzLhPjR8B&export=download"
+        )
+
+        archive_path = self.path + ".tar.bz2"
+
+        os.makedirs(self.path, exist_ok=True)
+        gdown.cached_download(url, archive_path)
+
+        with tarfile.open(archive_path, "r:bz2") as f:
+            f.extractall(os.path.dirname(archive_path))
+
+        os.remove(archive_path)
